e93dff7c44fb458adc25a6c511bafe7ab3bf7c0c
Link4Electronics <link4electronics@gmail.com>
2026-03-31 14:40:28 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.fooyin.fooyin.svg
 export DESKTOP=/usr/share/applications/org.fooyin.fooyin.desktop
+export DEPLOY_QT=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/fooyin
