029dd19213c38f0ea1c33a044f78c60742c3cbad
Link4Electronics <link4electronics@gmail.com>
2026-03-31 14:43:51 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,19 +7,23 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    libdecor       \
-    lxqt-qtplugin  \
-    kvantum        \
-    pipewire-audio \
-    pipewire-jack  \
-    qt6ct          \
-    sdl2
+    libdecor            \
+    lxqt-qtplugin       \
+    kdsingleapplication \
+    kvantum             \
+    pipewire-audio      \
+    pipewire-jack       \
+    qcoro               \
+    qt6ct               \
+    sdl2                \
+    taglib
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
+make-aur-package libvgm-git
 make-aur-package fooyin
 
 # If the application needs to be manually built that has to be done down here
